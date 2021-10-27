5b80670c92e553574c71a4e40c1575da6d260449
Monica Nataly <nata.moonk@gmail.com>
2021-10-27 15:46:42 -0500

    Adding resources of the course

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -6,5 +6,11 @@ class QuotesSpider(scrapy.Spider):
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
+#    def parse(self, response):
+#        pass
+
     def parse(self, response):
-        pass
+        h1_tag = response.xpath('//h1/a/text()').extract_first()
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+        yield {'H1 Tag': h1_tag, 'Tags': tags}
